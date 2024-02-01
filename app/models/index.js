ef8fe0d3795caa192647b32a9773f001cb3a82a8
Kelby Criss <128826649+sdrrfxc@users.noreply.github.com>
2024-02-01 13:30:38 -0600

    Got rid of student reference

--- app/models/index.js
+++ app/models/index.js
@@ -16,7 +16,6 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.user = require("./user.model.js")(sequelize, Sequelize);
 db.session = require("./session.model.js")(sequelize, Sequelize);
-db.student = require("./student.model.js")(sequelize, Sequelize);
 db.asset = require("./asset.model.js")(sequelize, Sequelize);
 db.assetdata = require("./assetdata.model.js")(sequelize, Sequelize);
 db.model = require("./model.model.js")(sequelize, Sequelize);
