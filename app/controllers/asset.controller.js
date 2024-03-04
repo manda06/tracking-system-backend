765bad11ca8a89c8eb3f34e89c42d86aa6158b74
sdrrfxc <kelby.criss@gmail.com>
2024-03-04 14:06:55 -0600

    Fixed assetData to refrence assetData instead of asset

--- app/controllers/asset.controller.js
+++ app/controllers/asset.controller.js
@@ -51,7 +51,7 @@ exports.findAll = (req, res) => {
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
   const id = req.params.id;
-  asset.findByPk(id, {include: Request})
+  asset.findByPk(id)
     .then((data) => {
       if (data) {
         res.send(data);
